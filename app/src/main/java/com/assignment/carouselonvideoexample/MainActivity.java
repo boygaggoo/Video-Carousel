c7630b112ade1a552e0930cb25092d0b0c630cbc
Muhammad Zia Shahid <ziagaggoo@gmail.com>
2018-06-22 09:57:07 +0500

    Setting volumn of video to 0
    After research on the library used for video streaming got solution to turn the volumn of video to 0

--- app/src/main/java/com/assignment/carouselonvideoexample/MainActivity.java
+++ app/src/main/java/com/assignment/carouselonvideoexample/MainActivity.java
@@ -35,5 +35,61 @@ public class MainActivity extends AppCompatActivity
         easyVideoPlayer.setLoop(true);
         easyVideoPlayer.hideControls();
         easyVideoPlayer.disableControls();
+        easyVideoPlayer.setCallback(new EasyVideoCallback()
+        {
+            @Override
+            public void onStarted(EasyVideoPlayer player)
+            {
+
+            }
+
+            @Override
+            public void onPaused(EasyVideoPlayer player)
+            {
+
+            }
+
+            @Override
+            public void onPreparing(EasyVideoPlayer player)
+            {
+
+            }
+
+            @Override
+            public void onPrepared(EasyVideoPlayer player)
+            {
+                player.setVolume(0f, 0f);
+            }
+
+            @Override
+            public void onBuffering(int percent)
+            {
+
+            }
+
+            @Override
+            public void onError(EasyVideoPlayer player, Exception e)
+            {
+
+            }
+
+            @Override
+            public void onCompletion(EasyVideoPlayer player)
+            {
+
+            }
+
+            @Override
+            public void onRetry(EasyVideoPlayer player, Uri source)
+            {
+
+            }
+
+            @Override
+            public void onSubmit(EasyVideoPlayer player, Uri source)
+            {
+
+            }
+        });
     }
 }
