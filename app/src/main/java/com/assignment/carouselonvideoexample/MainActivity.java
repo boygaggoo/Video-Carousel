b3c3d9a8e59c76b0c03bccaf18d698fbc0f4bea4
Muhammad Zia Shahid <ziagaggoo@gmail.com>
2018-06-22 09:50:48 +0500

    Configuring video streaming player
    Compiling 3rd party library in gradle for video streaming payer
    initializing and setting up video player in activity class

--- app/src/main/java/com/assignment/carouselonvideoexample/MainActivity.java
+++ app/src/main/java/com/assignment/carouselonvideoexample/MainActivity.java
@@ -1,15 +1,39 @@
 package com.assignment.carouselonvideoexample;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.afollestad.easyvideoplayer.EasyVideoCallback;
+import com.afollestad.easyvideoplayer.EasyVideoPlayer;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity
 {
+    private String URL = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
+
+    @BindView(R.id.easyVideoPlayer)
+    EasyVideoPlayer easyVideoPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initializeVideoPlayer();
+    }
+
+    private void initializeVideoPlayer()
+    {
+        easyVideoPlayer = (EasyVideoPlayer) findViewById(R.id.easyVideoPlayer);
+        easyVideoPlayer.setSource(Uri.parse(URL));
+        easyVideoPlayer.start();
+        easyVideoPlayer.setAutoPlay(true);
+        easyVideoPlayer.setLoop(true);
+        easyVideoPlayer.hideControls();
+        easyVideoPlayer.disableControls();
     }
 }
